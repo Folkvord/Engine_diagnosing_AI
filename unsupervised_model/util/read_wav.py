a56c68530d589948753b8110a68a4e631d1a51a0
FOLKVORD <folkvord.kristoffer@gmail.com>
2025-10-14 15:33:10 +0200

    util fuctions added

--- unsupervised_model/util/read_wav.py
+++ unsupervised_model/util/read_wav.py
@@ -12,6 +12,12 @@ def get_test_data() -> tuple:
     data = glob.glob("data/test_cut/*/*.wav")
     return data
 
+def process_wav_files(data: tuple) -> tuple:
+    processed_data = []
+    for wav_file in data:
+        processed_data.append(read_wav_with_sr(wav_file))
+    return tuple(processed_data)
+
 
 def read_wav_with_sr(file_as_path: str) -> Tuple[np.ndarray, int]:
     with wave.open(file_as_path) as wav_file:
