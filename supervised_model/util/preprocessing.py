c88cc22eb5a273820cf8957eb909619eb4e09171
247511 <247511@usn.no>
2025-10-14 15:55:26 +0200

    Update preprocessing.py

--- supervised_model/util/preprocessing.py
+++ supervised_model/util/preprocessing.py
@@ -2,25 +2,7 @@ import numpy as np
 import librosa
 
 def reduce_noise(y, sr, noise_duration=0.5, prop_decrease=0.9):
-    """
-    Enkel spektral støyreduksjon.
     
-    Parametere:
-    -----------
-    y : np.ndarray
-        Rå lyd (float32 i [-1, 1])
-    sr : int
-        Samplerate
-    noise_duration : float
-        Hvor mange sekunder i starten som brukes til å estimere støyprofil
-    prop_decrease : float
-        Hvor mye av støyen som skal fjernes (0–1, der 1 = mest demping)
-    
-    Returnerer:
-    -----------
-    y_denoised : np.ndarray
-        Lydsignal med redusert bakgrunnsstøy
-    """
     # 1. Estimer støyprofil fra starten (f.eks. 0.5 sek)
     n_noise = int(noise_duration * sr)
     noise_clip = y[:n_noise]
