6ce89c144044fe1e9b4e50ca301654c9249f37d0
FOLKVORD <folkvord.kristoffer@gmail.com>
2025-10-14 14:57:49 +0200

    util fix

---
+++ supervised_model/util/read_wav.py
@@ -0,0 +1,28 @@
+import numpy as np
+import wave
+
+def get_train_path(type: str) -> str:
+    if type == "good":
+        return "data/train_cut/engine1_good/"
+    elif type == "broken":
+        return "data/train_cut/engine2_broken/"
+    elif type == "heavy load":
+        return "data/train_cut/engine3_heavyload/"
+    else:
+        return ""
+
+def get_test_path(type: str) -> str:
+    if type == "good":
+        return "data/test_cut/engine1_good/"
+    elif type == "broken":
+        return "data/test_cut/engine2_broken/"
+    elif type == "heavy load":
+        return "data/test_cut/engine3_heavyload/"
+    else:
+        return ""
+
+def read_wav(file_as_path: str) -> np.ndarray:
+    with wave.open(file_as_path) as wav_file:
+        frames = wav_file.readframes(wav_file.getnframes())
+        audio_array = np.frombuffer(frames, dtype=np.int16)
+    return audio_array;
---
+++ unsupervised_model/kristoffer.py
@@ -0,0 +1,3 @@
+import util.read_wav as read_wav 
+
+print(read_wav.get_train_path("good"))
