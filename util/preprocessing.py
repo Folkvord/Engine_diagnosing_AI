9f8e9141b9ac8a366af610755da0184f420b801f
FOLKVORD <folkvord.kristoffer@gmail.com>
2025-10-15 13:55:37 +0200

    preprocess_data function

--- util/preprocessing.py
+++ util/preprocessing.py
@@ -8,8 +8,8 @@ def reduce_noise(audio_array: np.ndarray, sample_rate: int, noise_duration=0.5, 
     noise_clip = audio_array[:n_noise]
 
     # 2. Beregn STFT (frekvensrepresentasjon)
-    S_full, phase = librosa.magphase(librosa.stft(audio_array))
-    S_noise, _ = librosa.magphase(librosa.stft(noise_clip))
+    S_full, phase = librosa.magphase(librosa.stft(audio_array.astype(np.float32)))
+    S_noise, _ = librosa.magphase(librosa.stft(noise_clip.astype(np.float32)))
 
     # 3. Beregn gjennomsnittlig støyenergi per frekvens
     noise_profile = np.mean(S_noise, axis=1, keepdims=True)
@@ -24,3 +24,14 @@ def reduce_noise(audio_array: np.ndarray, sample_rate: int, noise_duration=0.5, 
     # 6. Normaliser og returner
     y_denoised = y_denoised / np.max(np.abs(y_denoised) + 1e-8)
     return y_denoised.astype(np.float32)
+
+
+# Processes an array of dataentries
+def preprocess_data(data: tuple) -> np.ndarray:
+    processed_data = []
+    for i in range(len(data)):
+        audio_array = data[i][0]
+        sample_rate = data[i][1]
+        processed_entry = reduce_noise(audio_array, sample_rate)
+        processed_data.append(processed_entry)
+    return processed_data
